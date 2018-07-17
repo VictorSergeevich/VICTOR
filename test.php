315505a54a485cdedf69438a3c59445047497f76
VICTOR <klinok92@inbox.ru>
2018-07-17 22:39:21 +0300

    Версия 1.0.1

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/*$languages = simplexml_load_file("import.xml");
+$mass = (array)$languages;
+foreach ($mass as $key => $item) {
+	foreach ($item as $k => $v) {
+		echo"<PRE>"; print_r($v->price); echo"</PRE>";
+	}
+}
+*/
+//echo"<PRE>"; print_r((array)$languages); echo"</PRE>";
+//phpinfo();
+echo "Здравствуйте! Ваш IP адрес: ".$_SERVER["REMOTE_ADDR"];
+?>
